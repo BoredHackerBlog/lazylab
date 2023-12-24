e67267a94a26a54eb61c28d016d575b65cae2f69
boredhackerblog <38662926+BoredHackerBlog@users.noreply.github.com>
2023-12-24 13:44:06 -0500

    Update add_task_dc.bat

--- scripts/add_task_dc.bat
+++ scripts/add_task_dc.bat
@@ -1,2 +1,2 @@
 curl https://live.sysinternals.com/PsExec64.exe --output c:\psexec.exe
-SCHTASKS /Create /TN check_ws /SC MINUTE /MO 5 /TR "c:\psexec -accepteula -i \\workstation1 -u lazylab\pam -p 123Password123 cmd.exe /c ping -n 30 127.0.0.1" /RU LazyLab.local\pam /RP 123Password123
+SCHTASKS /Create /TN check_ws /SC MINUTE /MO 5 /TR "c:\psexec -accepteula -i \\workstation1 -u lazylab\pam -p 123Password123 cmd.exe /c ping -n 60 127.0.0.1" /RU LazyLab.local\pam /RP 123Password123
